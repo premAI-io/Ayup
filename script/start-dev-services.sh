2172952e6d54d7b9e868faa45a690488c92af9dd
Richard Palethorpe <io@richiejp.com>
2024-07-25 09:48:23 +0100

    script: Use AYUP_CONTAINERD_ADDR when starting buildkitd

--- script/start-dev-services.sh
+++ script/start-dev-services.sh
@@ -2,4 +2,6 @@
 
 echo Starting buildkit
 
-buildkitd --oci-worker false --containerd-worker-addr /run/docker/containerd/containerd.sock
+: ${AYUP_CONTAINERD_ADDR:=/run/docker/containerd/containerd.sock}
+
+buildkitd --oci-worker false --containerd-worker-addr $AYUP_CONTAINERD_ADDR
