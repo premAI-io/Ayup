5135d0ba36b3a976221b3d7cbd0ce7746e2299ca
Richard Palethorpe <io@richiejp.com>
2024-08-01 11:19:22 +0100

    portForward: Prevent half closed connections by explicitly closing
    Previously we were allowing the port forward RPC stream to linger in a
    zombie state when one side of the forwarded connection had closed.
    
    So we now forcibly close the connection when reading on one side
    returns EOF or similar.
    
    Note: I haven't ruled out the read part of a connection could return
    EOF while the write is still open and expecting data. However the Go
    stdlib doesn't support partial shutdown of a connection.

--- go/cli/push/run.go
+++ go/cli/push/run.go
@@ -26,13 +26,20 @@ func (s *Pusher) startPortForwarder(ctx context.Context, wg *sync.WaitGroup) (ne
 
 	trace.Event(ctx, "TCP proxy listening on 5000")
 
-	egress := func(conn net.Conn, stream pb.Srv_ForwardClient) {
+	egress := func(ctx context.Context, wg *sync.WaitGroup, conn net.Conn, stream pb.Srv_ForwardClient) {
+		defer wg.Done()
+
 		buf := make([]byte, 16*1024)
 		for {
 			len, err := conn.Read(buf)
 			if err != nil {
 				if err != io.EOF {
 					terror.Ackf(ctx, "conn read: %w", err)
+				}
+
+
+				if err := stream.CloseSend(); err != nil {
+					terror.Ackf(ctx, "stream close send: %w", err)
 				}
 				return
 			}
@@ -50,14 +57,20 @@ func (s *Pusher) startPortForwarder(ctx context.Context, wg *sync.WaitGroup) (ne
 		}
 	}
 
-	ingress := func(conn net.Conn, stream pb.Srv_ForwardClient) {
+	ingress := func(ctx context.Context, wg *sync.WaitGroup, conn net.Conn, stream pb.Srv_ForwardClient) {
+		defer wg.Done()
+
 		for {
 			res, err := stream.Recv()
 			if err != nil {
 				if err != io.EOF {
 					terror.Ackf(ctx, "stream recv: %w", err)
 				}
-				return
+				break
+			}
+
+			if res.Closed {
+				break
 			}
 
 			trace.Event(ctx, "ingress stream recv")
@@ -71,6 +84,8 @@ func (s *Pusher) startPortForwarder(ctx context.Context, wg *sync.WaitGroup) (ne
 
 			trace.Event(ctx, "ingress conn write")
 		}
+
+		terror.Ackf(ctx, "conn close: %w", conn.Close())
 	}
 
 	handler := func(conn net.Conn) {
@@ -81,10 +96,16 @@ func (s *Pusher) startPortForwarder(ctx context.Context, wg *sync.WaitGroup) (ne
 			terror.Ackf(ctx, "client forward: %w", err)
 			return
 		}
-		defer func() { terror.Ackf(ctx, "stream closeSend: %w", stream.CloseSend()) }()
 
-		go ingress(conn, stream)
-		egress(conn, stream)
+		var wg sync.WaitGroup
+		wg.Add(2)
+
+		ctx := stream.Context()
+
+		go ingress(ctx, &wg, conn, stream)
+		go egress(ctx, &wg, conn, stream)
+
+		wg.Wait()
 
 		trace.Event(ctx, "conn done")
 	}
