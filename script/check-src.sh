b3c0c5174919564e81cd2f86c4e52b1dd5c54e56
Richard Palethorpe <io@richiejp.com>
2024-08-02 11:24:59 +0100

    ci: Add formatting check

--- script/check-src.sh
+++ script/check-src.sh
@@ -1,3 +1,9 @@
 #!/bin/sh -eu
 
 (cd go && golangci-lint run)
+
+need_fmt=$(gofmt -l -s ./go | wc -l)
+if [ "$need_fmt" -gt 0 ]; then
+    echo Files need formatting: $need_fmt, run '"gofmt -w -s ."'
+    exit 1
+fi
