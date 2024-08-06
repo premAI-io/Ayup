e93f38af6e1c2fa26397c5cd56daf141a7a59135
Richard Palethorpe <io@richiejp.com>
2024-08-06 18:31:42 +0100

    Merge build into analysis and run pipreqs inside a container
    This calls Buildkit from the analysis stage to run pipreqs in a
    container and then does the build immediately after.
    
    Meaning we no longer require pipreqs, but more importantly we can
    fluidly interleave any sort of build and analysis steps.

--- go/srv/analysis.go
+++ go/srv/analysis.go
@@ -3,16 +3,28 @@ package srv
 import (
 	"bufio"
 	"context"
+	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"regexp"
+	"strings"
 
 	tr "go.opentelemetry.io/otel/trace"
 	"google.golang.org/grpc"
 
 	pb "premai.io/Ayup/go/internal/grpc/srv"
 	"premai.io/Ayup/go/internal/trace"
+
+	"github.com/moby/buildkit/client"
+	"github.com/moby/buildkit/client/llb"
+	gateway "github.com/moby/buildkit/frontend/gateway/client"
+	"github.com/opencontainers/go-digest"
+
+	// gatewayapi "github.com/moby/buildkit/frontend/gateway/pb"
+	"github.com/tonistiigi/fsutil"
+
+	"premai.io/Ayup/go/internal/terror"
 )
 
 type ActServer interface {
@@ -21,16 +33,16 @@ type ActServer interface {
 	grpc.ServerStream
 }
 
-func (s *Srv) useDockerfile(ctx context.Context, stream pb.Srv_AnalysisServer) (bool, error) {
+func (s *Srv) useDockerfile(ctx context.Context, stream pb.Srv_AnalysisServer) error {
 	internalError := mkInternalError(ctx, stream)
 
 	_, err := os.Stat(filepath.Join(s.SrcDir, "Dockerfile"))
 	if err != nil {
 		if !os.IsNotExist(err) {
-			return false, internalError("stat Dockerfile: %w", err)
+			return internalError("stat Dockerfile: %w", err)
 		}
 
-		return false, nil
+		return nil
 	}
 
 	if err := stream.Send(&pb.ActReply{
@@ -39,23 +51,14 @@ func (s *Srv) useDockerfile(ctx context.Context, stream pb.Srv_AnalysisServer) (
 			Log: "Found Dockerfile, will use it",
 		},
 	}); err != nil {
-		return false, internalError("stream send: %w", err)
+		return internalError("stream send: %w", err)
 	}
 
 	s.push.analysis = &pb.AnalysisResult{
 		UseDockerfile: true,
 	}
 
-	if err := stream.Send(&pb.ActReply{
-		Source: "ayup",
-		Variant: &pb.ActReply_AnalysisResult{
-			AnalysisResult: s.push.analysis,
-		},
-	}); err != nil {
-		return false, internalError("stream send: %w", err)
-	}
-
-	return true, nil
+	return nil
 }
 
 func (s *Srv) Analysis(stream pb.Srv_AnalysisServer) error {
@@ -67,6 +70,11 @@ func (s *Srv) Analysis(stream pb.Srv_AnalysisServer) error {
 	internalError := mkInternalError(ctx, stream)
 
 	recvChan := mkRecvChan(ctx, stream)
+
+	c, err := client.New(ctx, s.BuildkitdAddr)
+	if err != nil {
+		return internalError("client new: %w", err)
+	}
 
 	// TODO: Check if we are dealing with an existing session etc.
 	r, ok := <-recvChan
@@ -85,13 +93,11 @@ func (s *Srv) Analysis(stream pb.Srv_AnalysisServer) error {
 		return sendError("premature choice")
 	}
 
-	if useDockerfile, err := s.useDockerfile(ctx, stream); useDockerfile || err != nil {
-		return err
-	}
-
 	requirements_path := filepath.Join(s.SrcDir, "requirements.txt")
 
-	if _, err := os.Stat(requirements_path); err != nil {
+	if err := s.useDockerfile(ctx, stream); err != nil {
+		return err
+	} else if _, err := os.Stat(requirements_path); err != nil {
 		ctx, span := trace.Span(ctx, "requirements")
 		defer span.End()
 
@@ -141,13 +147,57 @@ func (s *Srv) Analysis(stream pb.Srv_AnalysisServer) error {
 		}
 
 		span.AddEvent("Creating requirements.txt")
-		cmd := exec.Command("pipreqs", s.SrcDir)
 
-		procWait := mkProcWaiter(ctx, stream, recvChan)
-		in, out := startProc(ctx, cmd)
+		local := llb.Local("context", llb.ExcludePatterns([]string{".git"}))
+		st := pythonSlimLlb().File(llb.Copy(local, ".", "."))
+		st = pythonSlimPip(st, "install pipreqs").
+			Run(llb.Shlex("pipreqs")).Root()
 
-		if err = procWait("pipreqs", in, out); err != nil {
-			return err
+		dt, err := st.Marshal(ctx, llb.LinuxAmd64)
+		if err != nil {
+			return internalError("marshal: %w", err)
+		}
+
+		b := func(ctx context.Context, c gateway.Client) (*gateway.Result, error) {
+			r, err := c.Solve(ctx, gateway.SolveRequest{
+				Definition: dt.ToPB(),
+			})
+			if err != nil {
+				return nil, internalError("client solve: %w", err)
+			}
+
+			reqs, err := r.Ref.ReadFile(ctx, gateway.ReadRequest{
+				Filename: "/app/requirements.txt",
+			})
+			if err != nil {
+				return nil, terror.Errorf(ctx, "ref readfile: %w", err)
+			}
+
+			requirementsFile, err := os.OpenFile(requirements_path, os.O_CREATE|os.O_WRONLY, 0666)
+			if err != nil {
+				return nil, terror.Errorf(ctx, "openfile requirements: %w", err)
+			}
+			defer requirementsFile.Close()
+
+			if _, err := requirementsFile.Write(reqs); err != nil {
+				return nil, terror.Errorf(ctx, "requirementsFile write: %w", err)
+			}
+
+			return r, nil
+		}
+
+		contextFS, err := fsutil.NewFS(s.SrcDir)
+		if err != nil {
+			return internalError("fsutil newfs: %w", err)
+		}
+
+		statusChan := buildkitStatusSender(ctx, stream)
+		if _, err := c.Build(ctx, client.SolveOpt{
+			LocalMounts: map[string]fsutil.FS{
+				"context": contextFS,
+			},
+		}, "ayup", b, statusChan); err != nil {
+			return internalError("build: %w", err)
 		}
 	} else {
 		span.AddEvent("requirements.txt exists")
@@ -188,14 +238,228 @@ func (s *Srv) Analysis(stream pb.Srv_AnalysisServer) error {
 		}
 	}
 
-	if err := stream.Send(&pb.ActReply{
-		Source: "Ayup",
-		Variant: &pb.ActReply_AnalysisResult{
-			AnalysisResult: s.push.analysis,
-		},
-	}); err != nil {
-		return internalError("stream send: %w", err)
+	if s.push.analysis.UseDockerfile {
+		ctx, span := trace.Span(ctx, "buildctl")
+		defer span.End()
+
+		procWait := mkProcWaiter(ctx, stream, recvChan)
+
+		cmd := exec.Command(
+			"buildctl",
+			"--addr", s.BuildkitdAddr,
+			"build",
+			"--frontend=dockerfile.v0",
+			"--output", fmt.Sprintf("type=image,name=%s", s.ImgName),
+			"--local", fmt.Sprintf("context=%s", s.SrcDir),
+			"--local", fmt.Sprintf("dockerfile=%s", s.SrcDir),
+		)
+		cmd.Env = filterEnv(cmd)
+
+		in, out := startProc(ctx, cmd)
+
+		return procWait("buildctl", in, out)
+	}
+
+	if err = func() (err error) {
+		ctx, span := trace.Span(ctx, "build")
+		defer span.End()
+
+		internalError := mkInternalError(ctx, stream)
+
+		b := func(ctx context.Context, c gateway.Client) (*gateway.Result, error) {
+			def, err := s.MkLlb(ctx)
+			if err != nil {
+				return nil, internalError("mkllb: %w", err)
+			}
+
+			r, err := c.Solve(ctx, gateway.SolveRequest{
+				Definition: def.ToPB(),
+			})
+			if err != nil {
+				return nil, internalError("client solve: %w", err)
+			}
+
+			return r, nil
+		}
+
+		statusChan := buildkitStatusSender(ctx, stream)
+		contextFS, err := fsutil.NewFS(s.SrcDir)
+		if err != nil {
+			return internalError("fsutil newfs: %w", err)
+		}
+
+		_, err = c.Build(ctx, client.SolveOpt{
+			Exports: []client.ExportEntry{
+				{
+					Type: client.ExporterImage,
+					Attrs: map[string]string{
+						"name":        s.ImgName,
+						"push":        "false",
+						"unpack":      "false",
+						"compression": "uncompressed",
+					},
+				},
+			},
+			LocalMounts: map[string]fsutil.FS{
+				"context": contextFS,
+			},
+		}, "ayup", b, statusChan)
+
+		if err != nil {
+			return internalError("client build: %w", err)
+		}
+
+		return nil
+	}(); err != nil {
+		return err
+	}
+
+	if err := stream.Send(&pb.ActReply{}); err != nil {
+		return terror.Errorf(ctx, "stream send: %w", err)
 	}
 
 	return nil
+}
+
+func pythonSlimLlb() llb.State {
+	return llb.Image("docker.io/library/python:3.12-slim").
+		AddEnv("PYTHONUNBUFFERED", "True").
+		File(llb.Mkdir("/app", 0755)).
+		Dir("/app").
+		File(llb.Rm("/etc/apt/apt.conf.d/docker-clean"))
+}
+
+func pythonSlimPip(st llb.State, args string) llb.State {
+	pipCachePath := "/root/.cache/pip"
+	cachePipMnt := llb.AddMount(
+		pipCachePath,
+		llb.Scratch(),
+		llb.AsPersistentCacheDir(pipCachePath, llb.CacheMountLocked),
+	)
+
+	return st.Run(llb.Shlexf("pip %s", args), cachePipMnt).Root()
+}
+
+func (s *Srv) MkLlb(ctx context.Context) (*llb.Definition, error) {
+	local := llb.Local("context", llb.ExcludePatterns([]string{".venv", ".git"}))
+	st := pythonSlimLlb()
+
+	aptDeps := []string{}
+	if s.push.analysis.NeedsGit {
+		aptDeps = append(aptDeps, "git")
+	}
+
+	if s.push.analysis.NeedsLibGL {
+		aptDeps = append(aptDeps, "libgl1")
+	}
+
+	if s.push.analysis.NeedsLibGlib {
+		aptDeps = append(aptDeps, "libglib2.0-0")
+	}
+
+	if len(aptDeps) > 0 {
+		aptCachePath := "/var/cache/apt"
+
+		cacheAptMnt := llb.AddMount(
+			aptCachePath,
+			llb.Scratch(),
+			llb.AsPersistentCacheDir(aptCachePath, llb.CacheMountLocked),
+		)
+
+		st = st.Run(
+			llb.Shlexf(`dash -c "apt update && apt install -y %s"`, strings.Join(aptDeps, " ")),
+			cacheAptMnt,
+		).Root()
+	}
+
+	st = st.File(llb.Copy(local, "requirements.txt", "."))
+
+	pipCachePath := "/root/.cache/pip"
+	cachePipMnt := llb.AddMount(
+		pipCachePath,
+		llb.Scratch(),
+		llb.AsPersistentCacheDir(pipCachePath, llb.CacheMountLocked),
+	)
+	st = st.Run(llb.Shlex("pip install -r requirements.txt"), cachePipMnt).Root().
+		File(llb.Copy(local, ".", "."))
+
+	dt, err := st.Marshal(ctx, llb.LinuxAmd64)
+	if err != nil {
+		return nil, terror.Errorf(ctx, "marshal: %w", err)
+	}
+
+	return dt, nil
+}
+
+func filterEnv(cmd *exec.Cmd) []string {
+	// https://github.com/moby/moby/issues/46129#issuecomment-2016552967
+	var env []string
+	for _, kv := range cmd.Environ() {
+		if !strings.HasPrefix(kv, "OTEL_EXPORTER_OTLP_ENDPOINT=") {
+			env = append(env, kv)
+		}
+	}
+
+	return env
+}
+
+func buildkitStatusSender(ctx context.Context, stream pb.Srv_AnalysisServer) chan *client.SolveStatus {
+	statusChan := make(chan *client.SolveStatus)
+	sendLog := func(source string, text string) {
+		terror.Ackf(ctx, "send log stream send: %w", stream.Send(&pb.ActReply{
+			Source: source,
+			Variant: &pb.ActReply_Log{
+				Log: text,
+			},
+		}))
+	}
+
+	go func() {
+		verts := make(map[digest.Digest]int)
+
+		for msg := range statusChan {
+			for _, warn := range msg.Warnings {
+				sendLog("buildkit", fmt.Sprintf("Warning: %v", warn))
+			}
+			for _, vert := range msg.Vertexes {
+				vertNo, ok := verts[vert.Digest]
+				if !ok {
+					vertNo = len(verts) + 1
+					verts[vert.Digest] = vertNo
+				}
+
+				state := "NEW"
+				if vert.Started != nil {
+					state = "START"
+				}
+
+				if vert.Cached {
+					state = "CACHED"
+				} else if vert.Completed != nil {
+					state = "DONE"
+				}
+
+				duration := 0.0
+				if vert.Completed != nil && vert.Started != nil {
+					duration = vert.Completed.Sub(*vert.Started).Seconds()
+				}
+
+				if duration < 0.01 {
+					sendLog("buildkit", fmt.Sprintf("#%d %6s %s", vertNo, state, vert.Name))
+				} else {
+					sendLog("buildkit", fmt.Sprintf("#%d %6s %.2fs %s", vertNo, state, duration, vert.Name))
+				}
+			}
+			for _, log := range msg.Logs {
+				vertNo, ok := verts[log.Vertex]
+				if !ok {
+					vertNo = -1
+				}
+				sendLog("buildkit", fmt.Sprintf("#%d %6s %s", vertNo, "LOG", string(log.Data)))
+			}
+
+		}
+	}()
+
+	return statusChan
 }
